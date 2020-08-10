7112e2c69e0de7d4bbc37b5ada2180e03edc4e6f
Jemka12 <49412418+Jemka12@users.noreply.github.com>
2020-08-10 18:47:14 +0700

    Update preprocmodel.py

--- preprocmodel.py
+++ preprocmodel.py
@@ -255,4 +255,6 @@ class TarifClass():
 def load_model():
   os.system('gdown https://drive.google.com/uc?id=18lhZKdp1wrHcJmeidTqdsv_zSF1aQ54R&export=download')
   os.system('gdown https://drive.google.com/uc?id=1OQ9lYBwkXQUmT9ipw_ca1CaFuH1sBgg5&export=download')
+  os.system('gdown https://drive.google.com/uc?id=1--zQ9iXWpJKZV12aV1-GktPaeBq9aV8i&export=download')
+  
   print('Модели скачаны')
